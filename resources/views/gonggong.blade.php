5cfeef80711f22d3fa9233b328fda37f1f4c785a
cheyong <ngcheyong@gmail.com>
2023-10-15 08:05:46 +0800

    lets begin

---
+++ resources/views/gonggong.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="UTF-8">
+<title>Page Title</title>
+
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<link rel="stylesheet" href="">
+<style>
+</style>
+<script src=""></script>
+<body>
+
+
+<div class="">
+ <h1>Gonggong</h1>
+ {{ URL::asset('css/app.css'); }} 
+</div>
+
+</body>
+</html>
